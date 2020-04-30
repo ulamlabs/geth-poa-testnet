5f91312d71bc95022ce20f5479d09b0e77ca3249
Maciej Janiszewski <maciej@ulam.io>
2020-04-30 13:54:04 +0200

    Allow custom verbosity level

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 
+export ETH_VERBOSITY=${ETH_VERBOSITY:-1}
 export ETH_CHAIN_ID=${ETH_CHAIN_ID:-5555}
 
 replace_env() {
@@ -22,7 +23,7 @@ geth --datadir /data init ./.genesis.json
 geth --datadir /data account import --password /tmp/eth_pass /tmp/eth_private_key || true
 
 if [[ $# -eq 0 ]] ; then
-  exec geth --config .config.toml --allow-insecure-unlock --nousb --verbosity 5 --gcmode=archive --mine --miner.threads 1 --unlock $ETH_ADDRESS --password /tmp/eth_pass
+  exec geth --config .config.toml --allow-insecure-unlock --nousb --verbosity $ETH_VERBOSITY --gcmode=archive --mine --miner.threads 1 --unlock $ETH_ADDRESS --password /tmp/eth_pass
 else
   exec "$@"
 fi
