c0ca0ba3115993d9ca7818ec12a36b52e5ced3b7
Maciej Janiszewski <maciej@ulam.io>
2020-04-30 10:25:10 +0200

    Fix genesis.json issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 set -e
 
-sed -i "s|\$ETH_ADDRESS|$ETH_ADDRESS|g" genesis.json
-sed -i "s|\$ETH_BUFFER|$ETH_BUFFER|g" genesis.json
+# replace $ETH_... strings with env variable values without overwriting genesis.json
+cp genesis.json .genesis.json
+sed -i "s|\$ETH_ADDRESS|$ETH_ADDRESS|g" .genesis.json
+sed -i "s|\$ETH_BUFFER|$ETH_BUFFER|g" .genesis.json
 
 echo $ETH_PASSWORD > /tmp/eth_pass
 echo $ETH_PRIVATE_KEY > /tmp/eth_private_key
 
-geth --datadir /data init ./genesis.json
+geth --datadir /data init ./.genesis.json
 geth --datadir /data account import --password /tmp/eth_pass /tmp/eth_private_key || true
 
 if [[ $# -eq 0 ]] ; then
